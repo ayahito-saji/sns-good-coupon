6cdaea46bd7eaec2c9ea2aa537da9139930c74d6
sns-good-coupon <sns.good.coupon@gmail.com>
2018-07-10 09:58:20 +0900

    集計、出力、QRコードの実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,30 @@
 class HomeController < ApplicationController
+  require 'securerandom'
   def index
     if logged_in?
       @client = getClient(current_user)
+      @tweets_data = []
+      @favorite_total = 0
+      if @client
+        @match_data = /名古屋オーシャンズ|フットサル/
+        @client.user_timeline(count: 3000, tweet_mode: "extended" ).each do |tweet|
+          tweet_data = {
+              text: tweet.attrs[:full_text],
+              favorite_count: tweet.favorite_count,
+              retweet_count: tweet.retweet_count,
+              reply?: tweet.reply?,
+              retweet?: tweet.retweet?,
+              find_word?: !tweet.attrs[:full_text].scan(@match_data).empty?
+          }
+          puts "find word #{tweet.attrs[:full_text].scan(@match_data)}"
+          if tweet_data[:retweet?] == false && tweet_data[:find_word?] == true
+            @favorite_total += tweet_data[:favorite_count]
+            @tweets_data << tweet_data
+          end
+        end
+        @coupon_text = "スタジアム観戦無料"
+        @url_code = "https://sns-good-coupon.herokuapp.com/coupon-auth?id={#{0}}&p=#{SecureRandom.base58}"
+      end
     end
   end
 end
